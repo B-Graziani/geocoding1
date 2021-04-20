6f6e94711629605f05d7516fab37e87fb73f80a1
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-20 17:37:11 +0200

    done geocoding

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,15 @@ require("channels")
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { mapboxJS } from '../components/mapbox';
+import { initAutocomplete } from '../components/autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  mapboxJS();
+  initAutocomplete();
 });
